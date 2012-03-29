2bf0c8cc41d9a54a2460a4649b5d93f971d9d3c3
Ben Brewer <ben.brewer@razorfish.com>
2012-03-29 16:49:59 -0700

    moved to a page, outside of popup

--- content.js
+++ content.js
@@ -36,10 +36,45 @@ var fbt = {
 			$debug.toggle();
 		});
 
-		fbt.view.update('home');
+		$('#appAddFormGenerateToken').on('click', function(e) {
+			
+			e.preventDefault();
 
-		//fbt.FBGetAppAccessToken('245959388810540', 'c67c664d4fc79d5840dcedb0725a2fde');
-		//fbt.FBTestUsersGetByApp('245959388810540', '');
+			//todo: add loader
+			//todo: add more robust error handling
+			var appid = $('#appAddFormID').val();
+			var secret = $('#appAddFormSecret').val();			
+			if ((! appid) || (appid === "") || (! secret) || (secret === "")) {
+				alert("Please enter your app id and app secret before requesting the access token.");
+			} else {
+				fbt.facebook.getAppAccessToken(appid, secret, function(token) {
+					if (token) {
+						$('#appAddFormToken').val(token);	
+					} else {
+						alert("Unable to retrieve token.  Please check app id and app secret and try again.");
+					}
+				});
+			}
+		});
+
+		$('#selectApp').on('change', function(e) {
+			fbt.view.update('usersList');
+		});
+
+		fbt.view.update('home');
+	},
+
+	appDataGetById: function(id) {
+		var apps = JSON.parse(localStorage.getItem('apps'));
+		var len = apps.apps.length;
+		var data = {};
+		for (var i=0; i < len; i++) {
+			if (apps.apps[i]["appid"] == id) {
+				data = apps.apps[i];			
+				break;
+			} 			
+		}		
+		return data;
 	},
 
 	appListGet: function() {
@@ -57,8 +92,11 @@ var fbt = {
 		var name = $('#appAddFormName').val();
 		var appid = $('#appAddFormID').val();
 		var secret = $('#appAddFormSecret').val();
+		var token = $('#appAddFormToken').val();
 
-		fbt.appAddItem({ name:name, appid:appid, secret:secret });	
+		fbt.appAddItem({ name:name, appid:appid, secret:secret, token:token });	
+		fbt.view.update('home');
+		fbt.message('App added');					
 	},
 
 	appAddItem: function(item) {
@@ -67,35 +105,64 @@ var fbt = {
 			var apps = JSON.parse(localStorage.getItem('apps'));
 			apps.apps.push(item);
 			localStorage.setItem('apps', JSON.stringify(apps));			
-			fbt.view.update('home');
-			fbt.message('App added');			
 		}
-	},
-
-	FBTestUsersGetByApp: function(appid, token) {
-		var url = "https://graph.facebook.com/" + appid + "/accounts/test-users?access_token=" + token;
-  		fbt.debug(url);
-	},
-
-	FBGetAppAccessToken: function(appid, appsecret, callback) {
-		var url = "https://graph.facebook.com/oauth/access_token?client_id=" + appid + "&client_secret=" + appsecret + "&grant_type=client_credentials";
-		fbt.debug("FBGetAppAccessToken: " + url);
-		$.ajax({
-			url: url,
-			success: function(data) {
-				fbt.debug("FBGetAppAccessToken: response: " + data);
-				var token = data.split("=")[1];
-				callback(token);
-			},
-			error: function() {
-				fbt.debug("FBGetAppAccessToken: request error");
-				callback();
-			}
-		});
 	},
 
 	message: function(s) {
 		$('#message').html(s).show();
+	},
+
+	facebook: {
+		getTestUsersByAppId: function(appid, token, callback) {
+			var url = "https://graph.facebook.com/" + appid + "/accounts/test-users?access_token=" + token;
+	  		fbt.debug(url);
+			$.ajax({
+				url: url,
+				dataType: "json",
+				success: function(data) {
+					fbt.debug("getTestUsersByAppId: response: " + data);
+					callback(data);
+				},
+				error: function() {
+					fbt.debug("getTestUsersByAppId: request error");
+					callback();
+				}
+			});	  		
+		},
+
+		getAppAccessToken: function(appid, appsecret, callback) {
+			var url = "https://graph.facebook.com/oauth/access_token?client_id=" + appid + "&client_secret=" + appsecret + "&grant_type=client_credentials";
+			fbt.debug("getAppAccessToken: " + url);
+			$.ajax({
+				url: url,
+				success: function(data) {
+					fbt.debug("getAppAccessToken: response: " + data);
+					var token = data.split("=")[1];
+					callback(token);
+				},
+				error: function() {
+					fbt.debug("getAppAccessToken: request error");
+					callback();
+				}
+			});
+		},
+
+		getUserById: function(id, token, callback) {
+			var url = "https://graph.facebook.com/" + id + "/?access_token=" + token;
+	  		fbt.debug("getUserById: " + url);
+			$.ajax({
+				url: url,
+				dataType: "json",
+				success: function(data) {
+					fbt.debug("getUserById: response: " + data);
+					callback(data);
+				},
+				error: function() {
+					fbt.debug("getUserById: request error");
+					callback();
+				}
+			});	 
+		}
 	},
 
 	view: {
@@ -120,6 +187,43 @@ var fbt = {
 			$('.userForm').hide();
 			fbt.view[id]();
 			$('#' + id).show();
+		},
+
+		usersList: function() {
+			//todo: lame, do this with parameter passing
+			var id = $('#selectApp').find(":selected").val();
+			var appData = fbt.appDataGetById(id);
+			var list = $('#usersListListing');
+
+			list.empty();
+
+			$('#usersList h3').text("Test Users for " + appData.name);
+
+			fbt.facebook.getTestUsersByAppId(appData.appid, appData.token, function(data) {
+				if (data) {
+					console.log(data);
+					//id, access_token, login_url
+					$.each(data.data, function(index, value) {
+						fbt.facebook.getUserById(value.id, value.access_token, function(user) {
+
+							var html = '<div class="userListingHead"><span>' + user.name + '</span>';
+							html += '<button class="btnEditUser" data-id="' + user.id + '">Edit</button>';
+							html += '<button class="btnDeleteUser" data-id="' + user.id + '">Delete</button>';
+							html += '</div>';
+							html += '<table class="userListing"><tbody>';						
+							html += '<tr><td class="key">ID:</td><td><a href="https://www.facebook.com/profile.php?id=' + user.id + '" target="_blank">' + user.id + '</a></td></tr>';
+							html += '<tr><td class="key">Gender:</td><td>' + user.gender + '</td></tr>';
+							html += '<tr><td class="key">Login:</td><td><a href="' + value.login_url + '" target="_blank">Login Link</a></td></tr>';		
+							html += '<tr><td class="key">Access Token:</td><td>' + value.access_token + '</td></tr></tbody>';
+							html += '</table>';
+							list.append(html);
+						});
+					});
+				} else {
+					//todo: better error handling
+					alert("An error occurred with this request.");
+				}
+			});
 		}
 	}
 }
